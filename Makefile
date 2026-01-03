623bc2140d3c814b50e72ba976b222d32df0efe6
Sarah Schulte <git@sarah.engineer>
2026-01-03 06:17:22 -0800

    make it faster

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=g++
-CFLAGS=--std=c++23
+CFLAGS=--std=c++23 -O3
 
 logs: logs.cpp
 	$(CC) -o logs logs.cpp $(CFLAGS)
--- logs.cpp
+++ logs.cpp
@@ -1,21 +1,20 @@
-#include <iostream>
 #include <cassert>
 #include <iomanip>
+#include <iostream>
 #include <ranges>
 #include <string_view>
 
-#define A 54059 /* a prime */
-#define B 76963 /* another prime */
-#define C 86969 /* yet another prime */
+#define A 54059   /* a prime */
+#define B 76963   /* another prime */
+#define C 86969   /* yet another prime */
 #define FIRSTH 37 /* also prime */
-unsigned hash_str(const char* s)
-{
-   unsigned h = FIRSTH;
-   while (*s) {
-     h = (h * A) ^ (s[0] * B);
-     s++;
-   }
-   return h; // or return h % C;
+unsigned hash_str(const char *s) {
+  unsigned h = FIRSTH;
+  while (*s) {
+    h = (h * A) ^ (s[0] * B);
+    s++;
+  }
+  return h; // or return h % C;
 }
 
 int main() {
@@ -23,24 +22,30 @@ int main() {
   using std::operator""sv;
   constexpr auto delim{";"sv};
   constexpr auto word_delim{" "sv};
-  while(std::getline(std::cin, str)) {
-    /* std::cerr << str << std::endl; */
+
+  // I/O go brrr
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  while (std::getline(std::cin, str)) {
     std::string name;
     std::string color;
     std::string msg;
     int privmsg = -1;
     bool tags = false;
+    int idx = -1;
     for (const auto word : std::views::split(str, word_delim)) {
+      idx += 1;
       auto wordsv = std::string_view(word);
-      if(!tags) {
+      if (!tags) {
         tags = true;
         for (const auto section : std::views::split(wordsv, delim)) {
           auto sectionsv = std::string_view(section);
           if (sectionsv.starts_with("display-name"sv)) {
             sectionsv.remove_prefix(13);
             name = sectionsv;
-          }
-          else if (sectionsv.starts_with("color"sv)) {
+          } else if (sectionsv.starts_with("color"sv)) {
             sectionsv.remove_prefix(6);
             color = sectionsv;
           }
@@ -51,11 +56,13 @@ int main() {
         privmsg += 1;
         continue;
       }
-      if(privmsg > 0) {
+      if (!privmsg && idx > 2) {
+        break;
+      }
+      if (privmsg > 0) {
         if (msg.size()) {
           msg += " ";
-        }
-        else {
+        } else {
           wordsv.remove_prefix(1);
         }
         if (wordsv.size()) {
@@ -67,15 +74,14 @@ int main() {
       unsigned int r, g, b;
       if (color.size()) {
         assert(sscanf(color.c_str(), "#%2x%2x%2x", &r, &g, &b) == 3);
-      }
-      else {
+      } else {
         auto colorhash = hash_str(name.c_str());
         r = colorhash & 0xff;
         g = (colorhash >> 8) & 0xff;
         b = (colorhash >> 16) & 0xff;
       }
-      std::cout << "\x1b[38;2;" << r << ";"<< g << ";" << b << "m";
-      std::cout << name << "\033[0m" << ": " << msg << std::endl;
+      std::cout << "\x1b[38;2;" << r << ";" << g << ";" << b << "m";
+      std::cout << name << "\033[0m" << ": " << msg << "\n";
     }
   }
 }
